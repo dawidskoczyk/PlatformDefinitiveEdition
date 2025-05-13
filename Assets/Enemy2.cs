2c1da8197d4faf73ef1d520e99c444f94482274a
Daniel1nOw <87197476+Daniel1nOw@users.noreply.github.com>
2025-05-13 23:27:28 +0200

    asd123

--- Assets/Enemy2.cs
+++ Assets/Enemy2.cs
@@ -7,10 +7,12 @@ public class Enemy2 : MonoBehaviour
     public float Xright;
     [SerializeField] bool startDirRight = true;
     [SerializeField] LayerMask playerLayer;
+    [SerializeField] GameObject player;
 
     private int direction;
 
     bool canChangeDir = true;
+    bool canMove = true;
 
     Rigidbody2D rb;
 
@@ -28,31 +30,52 @@ public class Enemy2 : MonoBehaviour
 
     void Update()
     {
-        
 
-        if (transform.position.x < Xleft || transform.position.x > Xright)
+        if ((transform.position.x < Xleft || transform.position.x > Xright) && canChangeDir)
         {
             direction *= -1;
+
+            canChangeDir = false;
+            Invoke(nameof(CanChangeTrue), 1f);
         }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            canMove = false;
+            rb.AddForce(Vector2.one * 2, ForceMode2D.Impulse);
+
+            Invoke(nameof(CanMoveNow), 0.5f);
+        }
+
+        if(Mathf.Abs(player.transform.position.x - transform.position.x) < 3.5f)
+        {
+            if (player.transform.position.x > transform.position.x)
+                direction = 1;
+            else
+                direction = -1;
+        }
+        
+
+
     }
 
     private void FixedUpdate()
     {
-        rb.AddForce(Vector2.right * direction * speed, ForceMode2D.Force);
+        //rb.AddForce(Vector2.right * direction * speed, ForceMode2D.Force);
+
+        if(canMove)
+            rb.linearVelocity = Vector2.right * direction * speed;
     }
 
-    //private void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    if (!(collision.gameObject.tag == "Player"))
-    //    {
-    //        direction *= -1;
 
-    //        canChangeDir = false;
+    void CanChangeTrue()
+    {
+        canChangeDir = true;
+    }
 
-    //        print("kolizja");
-
-    //        return;
-    //    }
-    //}
+    void CanMoveNow()
+    {
+        canMove = true;
+    }
 
 }
